d8b21323b5aa1e92456a8000cdca65e5c66f44eb
drumilji-tech <74672375+drumilji-tech@users.noreply.github.com>
2020-11-23 11:22:18 +0530

    Update Water.py

--- Water.py
+++ Water.py
@@ -56,10 +56,11 @@ def main():
             Model = LinearRegression()
             Model.fit(x_train,y_train)
             y_pred = Model.predict(x_test)
+            result=['y_test','y_pred']
             st.write("R2 Value:",r2_score(y_test,y_pred).round(4))
             st.write("Mean Squared Error:",np.sqrt(mean_squared_error(y_test,y_pred).round(4)))
             st.header("Consumptions VS Total Charges")
-            st.line_chart(data[['Consumption','Total Charges']])
+            st.line_chart(result)
 
         
            
