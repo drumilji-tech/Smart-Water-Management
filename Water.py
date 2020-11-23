ee86c41e781a8660f1d6cd4c374e268b3233da60
drumilji-tech <74672375+drumilji-tech@users.noreply.github.com>
2020-11-23 11:29:10 +0530

    Update Water.py

--- Water.py
+++ Water.py
@@ -79,7 +79,7 @@ def main():
             y_pred = Model.predict(x_test)
             st.write("R2 Value:",r2_score(y_test,y_pred).round(4))
             st.write("Mean Squared Error:",np.sqrt(mean_squared_error(y_test,y_pred).round(4)))
-            st.line_chart(data[['Consumption','Total Charges']])
+            st.line_chart(data[['Consumption','Total Charges']],height=400)
 
             
     
@@ -95,7 +95,7 @@ def main():
             y_pred = model.predict(x_test)
             st.write("R2 Value:",r2_score(y_test,y_pred).round(4))
             st.write("Mean Squared Error:",np.sqrt(mean_squared_error(y_test,y_pred).round(4)))
-            st.line_chart(data[['Consumption','Total Charges']])
+            st.line_chart(data[['Consumption','Total Charges']],height=400)
 
        
     if Model == "Support Vector Machine":
@@ -112,7 +112,7 @@ def main():
             y_pred = model.predict(x_test)
             st.write("R2 Value:",r2_score(y_test,y_pred).round(4))
             st.write("Mean Squared Error:",np.sqrt(mean_squared_error(y_test,y_pred).round(4))) 
-            st.line_chart(data[['Consumption','Total Charges']])
+            st.line_chart(data[['Consumption','Total Charges']],height=400)
         
     
      
